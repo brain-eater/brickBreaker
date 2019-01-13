7976b631593a7c1849ae6c61b4b2cb2e51f6c8d8
brain-eater <34330361+brain-eater@users.noreply.github.com>
2019-01-14 01:15:03 +0530

    Ball is bouncing on all wall need to make it bounce when touched paddle.
    have to refactor and remove duplicate code in create and draw function
    in main.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const LEFT_ARROW = "ArrowLeft";
 const RIGHT_ARROW = "ArrowRight";
+const SCREEN_HEIGHT = 97;
+const SCREEN_WIDTH = 70;
 
 const getBall = document => document.getElementById("ball_1");
 const getScreen = document => document.getElementById("screen");
@@ -7,14 +9,6 @@ const getPaddle = document => document.getElementById("paddle_1");
 
 const addPrefix = function(value, prefix = "px") {
   return value + prefix;
-};
-
-const drawPaddle = function(document, paddle) {
-  let paddleDiv = getPaddle(document);
-  paddleDiv.style.bottom = addPrefix(paddle.bottom, "vh");
-  paddleDiv.style.left = addPrefix(paddle.left, "vw");
-  paddleDiv.style.width = addPrefix(paddle.width, "vw");
-  paddleDiv.style.height = addPrefix(paddle.height, "vh");
 };
 
 const handleEvents = function(document, paddle) {
@@ -28,12 +22,29 @@ const handleEvents = function(document, paddle) {
   drawPaddle(document, paddle);
 };
 
-const drawWall = function(document, wall) {
-  let wallDiv = document.getElementById("left_wall");
-  wallDiv.style.top = addPrefix(wall.top, "vw");
+const drawPaddle = function(document, paddle) {
+  let paddleDiv = getPaddle(document);
+  paddleDiv.style.bottom = addPrefix(paddle.bottom, "vh");
+  paddleDiv.style.left = addPrefix(paddle.left, "vw");
+  paddleDiv.style.width = addPrefix(paddle.width, "vw");
+  paddleDiv.style.height = addPrefix(paddle.height, "vh");
+};
+
+const drawWall = function(document, wall, wallId) {
+  console.log("hi");
+
+  let wallDiv = document.getElementById(wallId);
+  wallDiv.style.top = addPrefix(wall.top, "vh");
   wallDiv.style.left = addPrefix(wall.left, "vw");
-  wallDiv.style.width = addPrefix(wall.breadth, "vw");
-  wallDiv.style.height = addPrefix(wall.length, "vh");
+  wallDiv.style.width = addPrefix(wall.breadth, "vw ");
+  wallDiv.style.height = addPrefix(wall.height, "vh");
+};
+const drawBall = function(document, ball) {
+  let ballDiv = getBall(document);
+  ballDiv.style.top = addPrefix(ball.top, "vh");
+  ballDiv.style.left = addPrefix(ball.left, "vw");
+  ballDiv.style.width = addPrefix(ball.diameter, "vw");
+  ballDiv.style.height = addPrefix(ball.diameter, "vw");
 };
 
 const createWall = function(document, wall, id) {
@@ -42,7 +53,7 @@ const createWall = function(document, wall, id) {
   wallDiv.id = id;
   wallDiv.className = "wall";
   screen.appendChild(wallDiv);
-  drawWall(document, wall);
+  drawWall(document, wall, id);
   return wallDiv;
 };
 
@@ -56,14 +67,6 @@ const createPaddle = function(document, paddle) {
   return paddleDiv;
 };
 
-const drawBall = function(document, ball) {
-  let ballDiv = getBall(document);
-  ballDiv.style.top = addPrefix(ball.top, "vw");
-  ballDiv.style.left = addPrefix(ball.left, "vw");
-  ballDiv.style.width = addPrefix(ball.radius, "vw");
-  ballDiv.style.height = addPrefix(ball.radius, "vw");
-};
-
 const createBall = function(document, ball) {
   let screen = getScreen(document);
   let ballDiv = document.createElement("div");
@@ -75,23 +78,27 @@ const createBall = function(document, ball) {
 
 const createGameComponents = function(document) {
   let paddle = new Paddle(3, 30, 12, 3);
-  createPaddle(document, paddle);
   let ball = new Ball(7, 10, 3.5);
+  let leftWall = new Wall(0, 0, SCREEN_HEIGHT, 1, "left");
+  let rightWall = new Wall(0, SCREEN_WIDTH, SCREEN_HEIGHT, 1, "right");
+  let topWall = new Wall(0, 0, 1, SCREEN_WIDTH + 1, "top");
+  let bottomWall = new Wall(SCREEN_HEIGHT, 0, 1, SCREEN_WIDTH + 1, "bottom");
+  walls = [rightWall, leftWall, topWall, bottomWall];
+  createPaddle(document, paddle);
   createBall(document, ball);
-  let leftWall = new Wall(0, 0, 97, 1);
   createWall(document, leftWall, "left_wall");
-  let game = new Game(30, 0.08);
-  return { paddle, ball, game, leftWall };
+  createWall(document, rightWall, "right_wall");
+  createWall(document, topWall, "top_wall");
+  createWall(document, bottomWall, "bottom_wall");
+  let game = new Game(30, 0.2, paddle, ball, walls);
+  return game;
 };
 
 const intialize = function() {
-  let { paddle, paddleDiv, ball, ballDiv, game } = createGameComponents(
-    document
-  );
-  game.pointToDirection(ball, drawBall.bind(null, document, ball));
+  let game = createGameComponents(document);
+  game.shoot(drawBall.bind(null, document, game.ball));
   let screen = getScreen(document);
   screen.focus();
-  screen.onkeydown = handleEvents.bind(null, document, paddle);
+  screen.onkeydown = handleEvents.bind(null, document, game.paddle);
 };
-
 window.onload = intialize;
--- model.js
+++ model.js
@@ -1,3 +1,9 @@
+const isInVerticalRange = function(block1, block2) {
+  let isLeft = block1.rightEdge <= block2.left;
+  let isRight = block1.left <= block2.rightEdge;
+  return isLeft && isRight;
+};
+
 class Paddle {
   constructor(bottom, left, width, height) {
     this.bottom = bottom;
@@ -12,13 +18,24 @@ class Paddle {
   moveRight() {
     this.left = this.left + 10;
   }
+
+  isTouching(ball) {
+    return;
+  }
 }
 
 class Ball {
-  constructor(top, left, radius) {
+  constructor(top, left, diameter) {
     this.top = top;
     this.left = left;
-    this.radius = radius;
+    this.diameter = diameter;
+  }
+
+  get dimensions() {
+    let { top, left, diameter } = this;
+    let rightEdge = left + diameter;
+    let bottomEdge = top + diameter;
+    return { top, left, rightEdge, bottomEdge };
   }
 }
 
@@ -56,36 +73,71 @@ const getCordinatesDiff = function(displacement, direction) {
 };
 
 class Game {
-  constructor(ballDirection, ballSpeed) {
+  constructor(ballDirection, ballSpeed, paddle, ball, walls) {
     this.ballDirection = ballDirection;
     this.ballSpeed = ballSpeed;
+    this.paddle = paddle;
+    this.ball = ball;
+    this.walls = walls;
   }
 
-  moveBall(ball) {
+  moveBall() {
     let coordinatesChange = getCordinatesDiff(
       this.ballSpeed,
       this.ballDirection
     );
-    ball.left += coordinatesChange.xChange;
-    ball.top += coordinatesChange.yChange;
+
+    this.ball.left += coordinatesChange.xChange;
+    this.ball.top += coordinatesChange.yChange;
   }
 
-  pointToDirection(ball, callback) {
+  bounceBall() {
+    this.ballDirection = this.ballDirection + 90;
+    this.ballDirection %= 360;
+  }
+
+  shoot(callback) {
+    let lastTimeTouched = false;
+
     setInterval(
       function() {
-        this.moveBall(ball);
+        console.log(this.paddle.isTouching(this.ball));
+        let isTouchingWall = wall => wall.isTouching(this.ball);
+        let touchingWall = this.walls.filter(isTouchingWall)[0];
+        if (touchingWall && !lastTimeTouched) {
+          this.bounceBall();
+          lastTimeTouched = true;
+        }
+        if (!touchingWall) {
+          lastTimeTouched = false;
+        }
+        this.moveBall(this.ball);
         callback();
       }.bind(this),
-      10
+      7
     );
   }
 }
 
 class Wall {
-  constructor(top, left, length, breadth) {
+  constructor(top, left, height, breadth, screenSide) {
     this.top = top;
     this.left = left;
-    this.length = length;
+    this.height = height;
     this.breadth = breadth;
+    this.screenSide = screenSide;
+    this.rightEdge = left + breadth;
+    this.bottomEdge = top + height;
+  }
+  isTouching(ball) {
+    let ballDimensions = ball.dimensions;
+    let touchingSides = {
+      right: this.left <= ballDimensions.rightEdge,
+      left: this.rightEdge >= ballDimensions.left,
+      top: this.bottomEdge >= ballDimensions.top,
+      bottom: this.top <= ballDimensions.bottomEdge
+    };
+
+    return touchingSides[this.screenSide];
   }
 }
